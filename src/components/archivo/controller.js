56c4858e62341b7f6d2c12c47eb4fdd95a9d851a
Ignacio Silva <KJulo.c@gmail.com>
2022-08-28 01:50:32 -0400

    :sparkles: added crud of "archivo" (network, controller, store)

---
+++ src/components/archivo/controller.js
@@ -0,0 +1,104 @@
+const store = require("./store");
+const validator = require("../../validator");
+
+const currentComponent = "archivo";
+
+function getArchivo(id) {
+  return new Promise((resolve, reject) => {
+    if (id) {
+      resolve(store.getArchivo(id));
+    } else {
+      reject(new Error("No se ha especificado el id del archivo"));
+    }
+  });
+}
+
+function getArchivos(filterItems, ordersItems) {
+  return new Promise((resolve) => {
+    const query = {};
+
+    if (Object.keys(filterItems).length > 0) {
+      query.where = {};
+      Object.keys(filterItems).forEach((key) => {
+        const variableType = validator.getFieldType(currentComponent, key);
+
+        if (variableType === "String" && !key.startsWith("id")) {
+          query.where[key] = {
+            contains: filterItems[key],
+            mode: "insensitive",
+          };
+        } else if (variableType === "Int") {
+          query.where[key] = parseInt(filterItems[key], 10);
+        } else {
+          query.where[key] = filterItems[key];
+        }
+      });
+    }
+
+    query.orderBy = [];
+    if (ordersItems.length > 0) {
+      ordersItems.forEach((orderItem) => {
+        query.orderBy.push({ [orderItem.attribute]: orderItem.type });
+      });
+    } else {
+      query.orderBy = { id: "asc" };
+    }
+    resolve(store.getArchivos(query));
+  });
+}
+
+function createArchivo(idContenido, nombre, archivoUpload) {
+  return new Promise((resolve, reject) => {
+    if (!idContenido && !nombre && !archivoUpload) {
+      reject(new Error("[Archivo invalido] Faltan datos"));
+    } else {
+      const archivo = {
+        idContenido,
+        nombre,
+        archivoUpload,
+      };
+      if (validator.validateTypeVariablesModel(currentComponent, archivo)) {
+        resolve(store.createArchivo(archivo));
+      } else {
+        reject(new Error("[Archivo invalido] Error en datos"));
+      }
+    }
+  });
+}
+
+function updateArchivo(id, idContenido, nombre, archivoUpload) {
+  return new Promise((resolve, reject) => {
+    if (!id && !idContenido && !nombre && !archivoUpload) {
+      reject(new Error("[Archivo invalido] Faltan datos"));
+    } else {
+      const archivo = {
+        idContenido,
+        nombre,
+        archivoUpload,
+      };
+      if (validator.validateTypeVariablesModel(currentComponent, archivo)) {
+        resolve(store.updateArchivo(id, archivo));
+      } else {
+        reject(new Error("[Archivo invalido] Error en datos"));
+      }
+    }
+  });
+}
+
+function deleteArchivo(id) {
+  return new Promise((resolve, reject) => {
+    if (id) {
+      resolve(store.deleteArchivo(id));
+    } else {
+      reject(new Error("No se ha especificado el id del archivo"));
+    }
+  });
+}
+
+module.exports = {
+  getArchivo,
+  getArchivos,
+  createArchivo,
+  updateArchivo,
+  deleteArchivo,
+};
---
+++ src/components/archivo/network.js
@@ -0,0 +1,124 @@
+const express = require("express");
+
+const router = express.Router();
+
+const controller = require("./controller");
+const response = require("../../network/response");
+
+router.get("/:id", (req, res) => {
+  controller
+    .getArchivo(req.params)
+    .then((archivo) => {
+      response.success(req, res, archivo, null, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, "Error inesperado", null, 500, err);
+    });
+});
+
+router.get("/", (req, res) => {
+  const filterItems = {};
+  const orders = [];
+  let orderItems = [];
+
+  if (Object.keys(req.query).length !== 0) {
+    Object.keys(req.query).forEach((key) => {
+      if (
+        key !== "id" &&
+        key !== "offset" &&
+        key !== "limit" &&
+        key !== "orderBy"
+      ) {
+        filterItems[key] = req.query[key];
+      } else if (key === "orderBy") {
+        orderItems = req.query[key].split(",");
+        orderItems.forEach((orderItem) => {
+          const item = orderItem.split(" ");
+          orders.push({ attribute: item[0], type: item[1] });
+        });
+      }
+    });
+  }
+  controller
+    .getArchivos(filterItems, orders)
+    .then((archivos) => {
+      response.success(req, res, archivos, 200);
+    })
+    .catch((err) => {
+      console.log(err);
+      response.error(req, res, "Error inesperado", null, 500, err);
+    });
+});
+
+router.post("/", (req, res) => {
+  controller
+    .createArchivo(req.body.idContenido, req.body.nombre, req.body.archivo)
+    .then((archivoCreado) => {
+      if (archivoCreado.catchError) {
+        response.error(
+          req,
+          res,
+          "Error inesperado",
+          null,
+          500,
+          archivoCreado.meta.cause
+        );
+      } else {
+        response.success(req, res, archivoCreado, null, 201);
+      }
+    })
+    .catch((err) => {
+      response.error(req, res, "Error inesperado", null, 500, err);
+    });
+});
+
+router.patch("/:id", (req, res) => {
+  controller
+    .updateArchivo(
+      req.params.id,
+      req.body.id_contenido,
+      req.body.nombre,
+      req.body.archivo
+    )
+    .then((updateArchivo) => {
+      if (updateArchivo.catchError) {
+        response.error(
+          req,
+          res,
+          "Error inesperado",
+          null,
+          500,
+          updateArchivo.meta.cause
+        );
+      } else {
+        response.success(req, res, updateArchivo, null, 200);
+      }
+    })
+    .catch((err) => {
+      response.error(req, res, "Error inesperado", null, 500, err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  controller
+    .deleteArchivo(req.params.id)
+    .then((archivoEliminado) => {
+      if (archivoEliminado.catchError) {
+        response.error(
+          req,
+          res,
+          "Error inesperado",
+          null,
+          500,
+          archivoEliminado.meta.cause
+        );
+      } else {
+        response.success(req, res, archivoEliminado, null, 200);
+      }
+    })
+    .catch((err) => {
+      response.error(req, res, "Error inesperado", null, 500, err);
+    });
+});
+
+module.exports = router;
