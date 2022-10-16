5017875d9f2b91760c21041aae05befe719f3be4
Ignacio Silva <KJulo.c@gmail.com>
2022-10-16 03:17:46 -0300

    :sparkles: update prisma schema

---
+++ prisma/migrations/20221016061720_16_10_22_3_17am/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `hora` on the `notificacion` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "notificacion" DROP COLUMN "hora";
