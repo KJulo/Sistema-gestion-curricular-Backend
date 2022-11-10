3f9b44926560a04270dc4a8ea5b27b424ce6d0ce
Ignacio Silva <KJulo.c@gmail.com>
2022-11-09 23:42:20 -0300

    logs heroku login

--- src/components/usuario/network.js
+++ src/components/usuario/network.js
@@ -22,6 +22,7 @@ router.post("/login", (req, res) => {
       }
     })
     .catch((err) => {
+      console.log(err)
       response.error(req, res, "Error inesperado", null, 500, err);
     });
 });
